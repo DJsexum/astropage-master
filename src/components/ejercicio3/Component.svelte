a6d7cfbca09eea852869753ae91fa6ea0a48a453
Juanchi <juancruzcabrerasocin@gmail.com>
2026-04-01 14:01:52 -0300

    Revisar y arreglar errores

--- src/components/ejercicio3/Component.svelte
+++ src/components/ejercicio3/Component.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 
     import { onMount } from "svelte";
+    import type { Post } from "../../core/interfaces/ejercicio3.interface";
 
-    let usuarios = $state <any[]>([]);
+    let posts = $state <Post[]>([]);
+    let userID = $state <number>(0);
 
     onMount(async () => 
     {
@@ -11,13 +13,49 @@
 
         const respuesta = await fetch
         (
-            "https://jsonplaceholder.typicode.com/users"
+            "https://jsonplaceholder.typicode.com/posts"
         );
-        const data = await respuesta.json();
-        usuarios = data;
+        
+        posts  = await respuesta.json();
+        console.log("posts: ", posts);
     });
 
+    const filterPosts = (userId: number) => 
+    {
+        if (userID === 0 || isNaN(userId)) 
+        {
+            alert("Por favor, ingresa un ID de usuario válido.");
+            return;
+        }
+
+        const filteredPosts = Post
+        .filter ((post) => post.userId === userId);
+        .map ((post) => 
+        {
+            return 
+            {
+                id: post.id,
+                title: post.title,
+            };
+        }
+            if (filterPosts.length === 0)
+            {
+                console.log("No se encontraron posts para el ID de usuario: " + userID);
+            }
+                else
+                {
+                    console.log("se encontraron " + filterPosts.length + " posts para el ID de usuario: " + userID);
+                }
+    };
+
 </script>
+
+<div class="mb-2 p-2">
+
+    <input class="border" bind:value={userID} type="number" step="1" min="0"/>
+    <button onclick = {() => filterPosts (userID)}> Filtrar Post </button>
+
+</div>
 
 <table class = "w-full">
 
@@ -26,9 +64,9 @@
         <tr>
 
             <th>ID</th>
-            <th>Nombre</th>
-            <th>Username</th>
-            <th>Email</th>
+            <th>UserId</th>
+            <th>Title</th>
+            <th>Body</th>
 
         </tr>
 
@@ -36,19 +74,19 @@
 
     <tbody class = "bg-gray-200 flex-1">
 
-        {#each usuarios as user}
+        {#each posts as post}
 
             <tr class = "p-2 hover:bg-gray-300">
 
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.username}</td>
-                <td>{user.email}</td>
+                <td>{post.id}</td>
+                <td>{post.userId}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
 
             </tr>
 
         {/each}
 
     </tbody>
-    
+
 </table>
