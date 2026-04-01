d90ea2cd1fbae4237d0cdb88861871463168b3d8
Juanchi <juancruzcabrerasocin@gmail.com>
2026-03-31 22:15:19 -0300

    Cualquiera

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav class ="bg-blue-700 text-white p-1">
+
+    <a class = "font-blod line underline m-2" href="/"> Ejercicio 1</a>
+    <a class = "font-blod line underline m-2" href ="/ej2"> Ejercicio 2</a>
+    <a class = "font-blod line underline m-2" href ="/ej3"> Ejercicio 3</a>
+    
+</nav>
---
+++ src/components/ejercicio3/Component.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+
+    import { onMount } from "svelte";
+
+    let usuarios = $state <any[]>([]);
+
+    onMount(async () => 
+    {
+
+        console.log("onMount");
+
+        const respuesta = await fetch
+        (
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await respuesta.json();
+        usuarios = data;
+    });
+
+</script>
+
+<table class = "w-full">
+
+    <thead class = "bg-gray-400 text-black flex-1">
+
+        <tr>
+
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Username</th>
+            <th>Email</th>
+
+        </tr>
+
+    </thead>
+
+    <tbody class = "bg-gray-200 flex-1">
+
+        {#each usuarios as user}
+
+            <tr class = "p-2 hover:bg-gray-300">
+
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+
+            </tr>
+
+        {/each}
+
+    </tbody>
+    
+</table>
